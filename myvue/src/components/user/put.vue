ad0ecf233c9be5379c8b93f16a428025e2ecaf6b
tallpeekaboo <1250511455@qq.com>
2021-03-05 15:32:17 +0800

    update

--- myvue/src/components/user/put.vue
+++ myvue/src/components/user/put.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        
+        <table border="1" collspacing='0' width='80%'>
+           <tr>
+               <th>用户名</th>
+               <th>密&emsp;码</th>
+               <th>改密码</th>
+           </tr>
+           <tr v-for="item in user_list" :key="item.id">
+               <th>{{item.username}}</th>
+               <th>**********</th>
+               <th><a-button type="primary" @click="newput(item.id)">改密码</a-button></th>
+           </tr>
+       </table>
     </div>
 </template>
 
@@ -9,14 +20,38 @@ import axios from 'axios'
 export default {
     data() {
         return {
-
+            user_list:[],
+            newpassword:"",
         }
     },
     methods: {
-
+      get_user(){
+          axios({
+              url:'http://127.0.0.1:8000/newapp/userinfo/',
+              method:'get',
+          }).then(resp=>{
+              console.log(resp.data)
+              this.user_list = resp.data
+          })
+      },
+      newput(uid){
+          var password1 = prompt('newpassword',this.newpassword)
+          axios({
+              url:'http://127.0.0.1:8000/newapp/userinfo/',
+              method:'put',
+              params:{
+                  uid:uid,
+                  password:password1,
+              }
+          }).then(resp=>{
+              console.log(resp.data)
+              alert(resp.data.msg)
+              this.$router.go(0)
+          })
+      }
     },
     created() {
-
+        this.get_user()
     }
 }
 </script>
